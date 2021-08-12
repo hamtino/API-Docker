f4f743338e8ea61eee859ff126215c8f5350e039
Juan Jaminton Yate Pelaez <50279936+hamtino@users.noreply.github.com>
2021-08-12 12:06:44 -0500

    inicializacion de sequelize con postgresql

--- index.js
+++ index.js
@@ -19,12 +19,29 @@ const HapiSwagger = require("hapi-swagger");
 // puerto de funcionamiento aplicacion node.js e inicializacion de hapi.js
 const port = process.env.PORT || 3000;
 const server = new Hapi.Server(
-  {
-    port
-  }
+    {
+        port
+    }
 );
 
 // declaramos la funcion tipo flecha principal de la aplicacion API
 (async () => {
+    // validamos datos correctos de postgresql
+    if (!process.env.POSTGRES_HOST) {
+        throw Error(
+            "process.env.POSTGRES_HOST debe ser: user:pass@ipService:port ",
+        );
+    }
+
+    // Configuramos sequelize para que se conecte a postgresql
+    const sequelize = new Sequelize(
+        `postgres://${process.env.POSTGRES_HOST}/${process.env.POSTGRES_DB || "personas"}`,
+        {
+            ssl: process.env.POSTGRES_SSL,
+            dialectOptions: {
+                ssl: process.env.POSTGRES_SSL,
+            },
+        }
+    );
 
 })();
